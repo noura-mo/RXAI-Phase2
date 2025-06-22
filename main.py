2c1779c1f3c60925cc38c9a9af42869dea85d3df
Nora Mohamed <ne263014@gmail.com>
2025-06-22 03:56:40 +0300

    main.py

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ def match(data: InputData):
 # تشغيل السيرفر في حالة local فقط
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
